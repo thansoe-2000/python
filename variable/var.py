79baf9b50866d08946cd8ca144e1dc02785d9b28
thansoe-2000 <tt6289216@gmail.com>
2024-09-30 20:30:52 -0700

    second local scope

--- variable/var.py
+++ variable/var.py
@@ -3,3 +3,9 @@ global_var= 'I am a global scope'
 def access_global_var():
     print(global_var)
 access_global_var()
+
+# local scope
+def function_with_local_var():
+    local_var = "I am a local variable"
+    print(local_var)
+function_with_local_var()
