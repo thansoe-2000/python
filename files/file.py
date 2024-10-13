4cbab9705308c79406fd764e47d51e926f75a6a4
Student <tt6289216@gmail.com>
2024-10-12 20:39:51 -0700

    create many variable

--- files/file.py
+++ files/file.py
@@ -1,7 +1,15 @@
 
 
 
-text = "Mra Pann Khine\nGood Night\nPann Chay\nHello, Mra Pann"
+text = "Python Group\nKyaw Soe Hla\nKyaw Lynn Awng\nMin Hein Zaw\nMra Pann Khine\nZaw Myo Naing\nThan Than Soe"
 with open('text.txt', 'w') as file:
     file.write(text)
+    
+name = "Than Than Soe"
+with open("text.txt", 'w') as file:
+    file.write(name)
+    
+fruit = "Apple"
+with open("text.tex", 'w') as file:
+    file.write(fruit)
     
