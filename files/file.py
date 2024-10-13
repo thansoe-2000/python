77547d87a10a410e04aa13b03f22d91976b581de
Student <tt6289216@gmail.com>
2024-10-12 20:25:14 -0700

    python files

---
+++ files/file.py
@@ -0,0 +1,7 @@
+
+
+
+text = "Mra Pann Khine\nGood Night\nPann Chay\nHello, Mra Pann"
+with open('text.txt', 'w') as file:
+    file.write(text)
+    
---
+++ files/files.py
@@ -0,0 +1,7 @@
+# r = read
+# a = append
+# w = write
+# x = create
+
+file = open("names.txt")
+print(file.read())
